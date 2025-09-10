3c1d613442ab926407c2a5a8834aaf277f06f161
sawan-sawan <evilistno@gmail.com>
2025-09-10 03:03:02 -0900

    social changes

--- src/Components/SocialPage.jsx
+++ src/Components/SocialPage.jsx
@@ -64,19 +64,19 @@ export default function SocialPage() {
                     viewport={{ once: true }}
                 >
                     <a href="https://www.youtube.com" target="_blank" rel="noreferrer" className="social-btn">
-                        <img src="/img/youtube.png" alt="YouTube" />
+                        <img className="iconimg" src="/img/youtube.png" alt="YouTube" />
                         <span>YouTube</span>
                     </a>
                     <a href="https://www.facebook.com" target="_blank" rel="noreferrer" className="social-btn">
-                        <img src="/img/facebook.png" alt="Facebook" />
+                        <img className="iconimg" src="/img/facebook.png" alt="Facebook" />
                         <span>Facebook</span>
                     </a>
                     <a href="https://www.instagram.com" target="_blank" rel="noreferrer" className="social-btn">
-                        <img src="/img/instagram.png" alt="Instagram" />
+                        <img className="iconimg" src="/img/instagram.png" alt="Instagram" />
                         <span>Instagram</span>
                     </a>
                     <a href="https://www.snapchat.com" target="_blank" rel="noreferrer" className="social-btn">
-                        <img src="/img/snapchat.png" alt="Snapchat" />
+                        <img className="iconimg" src="/img/snapchat.png" alt="Snapchat" />
                         <span>Snapchat</span>
                     </a>
                 </motion.div>
